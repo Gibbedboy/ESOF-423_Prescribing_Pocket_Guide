5b1fe09486c0b20d55c5facc9c62d23a1acf7413
Chenwei Cao <ccw5241@gmail.com>
2018-02-02 10:20:31 -0700

    creat a simple test for MedApp

---
+++ test.java
@@ -0,0 +1,15 @@
+//Junit to test if the password is correct style
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+@test
+public class testMedApp {
+      //TextField text = new TextField();
+      exception.expect(MedApp.class);
+      exception.expectMessage("Negative number not allowed");
+      MedApp t = new MedApp();
+      t.methodToBeTest(-1);
+    }
+  }
+}
