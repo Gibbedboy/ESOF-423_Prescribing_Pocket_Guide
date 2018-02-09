d5b89d78538f349aaacf56d60c3ecb418ecf969f
Gibbedboy <31701378+Gibbedboy@users.noreply.github.com>
2018-02-09 08:27:17 -0700

    Added PHQ9 Diagnosis
    No formatting or anything, just purely the functions to print out a diagnosis for a new patient.

---
+++ PHQ9Diag/PHQ9Diag.js
@@ -0,0 +1,72 @@
+// PHQ-9 Diagnosis Questionnaire Scoring
+
+var questionNum = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+
+function initializeQuestions() {
+  let doc = document.getElementById("Questionaire");
+  
+  for (let i = 0; i < doc.length; i++) {
+    questionNum[i] = doc.elements[i].value;
+  }
+  diagnose();
+}
+
+function diagnose() {
+  let score = 0;
+  // Total the scores
+  if (questionNum[0] > 1 || questionNum[1] > 1) {
+    score++;
+  }
+  if (totalSymptomCount() >= 5) {
+    score++;
+  }
+  if (questionNum[9] > 0) {
+    score++;
+    document.write("<br>Question 9 was posistive. Assess for suicide risk.<br>");
+  }
+  // If the answer is YES to 1,2, and 3, tentative diagnosis of depression
+  if (score > 2) {
+    document.write("<br>Tentative diagnosis of depression<br>");
+  }
+  document.write("<br>Severity Score:  ");
+  calcSeverity();
+}
+
+function totalSymptomCount() {
+  let total = 0;
+  for (let i = 0; i < 8; i++) {
+    if (questionNum[i] >= 2) {
+      total++;
+    }
+  }
+  if (questionNum[8] > 0) {
+    total++;
+  }
+  return total;
+}
+
+function calcSeverity() {
+  let total = 0;
+  for (let i = 0; i < 9; i++) {
+    total = Number(total) + Number(questionNum[i]);
+  }
+  document.write(total + "<br>");
+
+  // Score is 5-9
+  if (total > 5 && total < 10) {
+    document.write("<br> Minimal symptoms. Suport, ask to call if condition worsens, and return in 1 month. <br>");
+  }
+  // Score is 10-14
+  else if (total > 9 && total < 15) {
+    document.write("<br> Minor Depression Dysmthymia, or Major Depression Mild. Support, contact in one week. Antidepressent or psychotherarpy, contact in one week. <br>");
+  }
+  // Score is 15-19
+  else if (total > 14 && total < 20) {
+    document.write("<br> Major Depression, moderate. Antidepressent or psychotherapy. <br>");
+  }
+  // Score is over 20
+  else if (total >= 20) {
+    document
+    .write("<br> Major Depression, severe. Recommend antidepressants and psychotherapy (Especially if not improved on monotherapy). <br>");
+  }
+}
