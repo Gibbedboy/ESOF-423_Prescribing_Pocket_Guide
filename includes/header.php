ae82b8f7d02bf27aa9dd26b99df7fa4f4895a17c
Jordan Parr <rohmannjk@yahoo.com>
2018-03-01 13:05:35 -0700

    Add files via upload
    v0.5: further modified questionnaire on DB
    added user specific patients list
    added patient specific questionnaires adding to DB
    started patient class where a lot of code will be moving to

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<?php 
+	session_start(); 
+	$userID = $_SESSION['userID'];
+?>
+<head>
+	<meta charset="UTF-8">
+	<title><?php include('includes/titles.php') ?> | Prescribing Pocket Guide: Online</title>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css?t=<?=time()?>">
+	<!--<link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+	-->
+	<link rel="shortcut icon" href="favicon.ico?t=<?=time()?>" type="image/ico" />
+	
+</head>
+<body>
+	<div id="cm-container">
+		<header>
+			<div class="logo">
+				<a href="index.php"><img src="favicon.ico?t=<?=time()?>" height="50">
+				<h1>Prescribing Pocket Guide: Online</h1> </a>
+			</div>
+			<?php include('includes/nav.php'); ?>
+		</header>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<?php 
+$navlink = "login";
+include("includes/dbLogin.php"); 
+?>
+
+<html div id="bg">
+<head>
+	<meta charset="UTF-8">
+	<title><?php include('includes/titles.php') ?> | Prescribing Pocket Guide: Online</title>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css?t=<?=time()?>">
+	<!--<link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+	-->
+	<link rel="shortcut icon" href="favicon.ico?t=<?=time()?>" type="image/ico" />
+	
+</head>
+
+<body>
+	<div id="cm-container">
+		<header>
+			<div class="logo">
+				<a href="index.php"><img src="favicon.ico?t=<?=time()?>" height="50">
+				<h1>Prescribing Pocket Guide: Online</h1> </a>
+			</div>
+		</header>
+	</div>
+	<div class="mainContent">
+	<script type="text/javascript" src="assets/js/login.js?t=<?=time()?>"></script>
+	<div class = "WelcomeBox">
+		<h1>Welcome</h1>
+		<div class = "LoginBox">
+			<div class = "SubLogin">
+				<form name="name">
+					<input type="text" id="username" name="username" placeholder="Username">
+				</form>
+			</div>
+			<div class = "SubLogin">
+				<form name="pass" onsubmit="return false;" onkeypress="submitTester(this,event);">
+					<input type="password" id="password" name="password" placeholder="Password" >
+					<input type="submit" style="display: none" />
+				</form>
+			</div>
+			
+			<div class = "InlineLogin">
+				<a href="signup.php">Signup</a>
+			</div>
+			<div class = "InlineLogin">
+				<a href="#">Forgot Password</a>
+			</div>
+		</div>
+		<input type="button" class="button" value="Login" onClick="login();">
+	</div>
+	</div>
+</body>
+</html>
