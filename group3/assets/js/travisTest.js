ff01aa3a95d1a72aecfb95b1fa4e628ce77a42ef
Chenwei Cao <ccw5241@gmail.com>
2018-03-08 13:59:32 -0700

    added testTravis stuff

--- group3/assets/js/travisTest.js
+++ group3/assets/js/travisTest.js
@@ -1,3 +1,10 @@
-function test(input){
-	return input+1;
-}
+
+
+gulp.task('travis',['build', 'testServerJS'],function(){
+	process.exit(0);
+});
+
+
+gulp.task('reload', function(){
+	livereload.reload();
+});
